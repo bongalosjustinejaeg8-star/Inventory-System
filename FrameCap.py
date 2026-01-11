2e6ed079238e676e38df1c7773d4236b861f61c8
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-01-11 18:10:32 +0800

    X

--- FrameCap.py
+++ FrameCap.py
@@ -1,8 +1,10 @@
 
 from openpyxl import load_workbook
-file=load_workbook("DatabaseCap.xlsx").active
-Data = {}
-for item, qty in file.iter_rows(min_row=2, values_only=1):
+file=load_workbook("DatabaseCap.xlsx")
+file2 = file.active
+
+file2.append([input("enter product name: "),int(input("enter stocks"))])
     
-
-
+file.save("DatabaseCap.xlsx")
+for f,q in file2.iter_rows(min_row=2,values_only=1):
+    print(f,q)
