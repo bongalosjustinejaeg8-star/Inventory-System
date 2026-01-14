dd5834c3e5c844990a0b8f38b8f77f4b9dddfd3d
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-01-14 14:18:38 +0800

    dorongon helped me on iteration on for loops in order to find the items correctly

--- FrameCap.py
+++ FrameCap.py
@@ -1,4 +1,3 @@
-
 from openpyxl import load_workbook
 file=load_workbook("DatabaseCap.xlsx")
 file2 = file.active
@@ -6,42 +5,52 @@ file2 = file.active
 
 def add_new():
     file2.append([input("enter product name: "),int(input("enter stocks: "))])
+    file.save("DatabaseCap.xlsx")
     
 
 
 def change_stock():
-    product_name = input("enter product name: ").title()
-    quantity = int(input("enter new quantity: "))
-    for rows in file2.iter_rows(min_row=2,values_only=True):
-        if row[0] == product_name:
-            rows[1]= quantity
+    product_name = input("enter product name: ")
+    found=False
+    for rows in file2.iter_rows(min_row=2):
+        if rows[0].value == product_name:
+            found=True
+            rows[1].value = int(input("enter new quantity: "))
             file.save("DatabaseCap.xlsx")
             print ("stock updated")
             
             break
-        else: 
-            print("item not found")
-            descicion = input("press any character to exit or A to add new product: ").upper()
-            if descicion == "A":
-                add_new()
-            else:
-                break
+    if not found:
+        print(f"{product_name} not found")    
 
 def remv_item():
-    item = input("Enter Item Id to remove: ").title()
-    for row in file2.iter_rows(min_row=2,values_only=True):
-        if row[0] == item:
-            decision = input(f"are you sure you want to remove {row[0]}?(y/n): ").lower()
-            if decision == "y":
-                row[0]= None
-                row[1]=None
-                break
-            else:
-                remv_item() 
-            d = input("item id not found, wanna try again? y/n: ").lower()
-            if d == "y":
-                remv_item()
+    item = input("Enter Item ID to remove: ").title()
+    found = False
 
-for row in file2.iter_rows(min_row=2):
-    print(row[0].value,row[1].value)
-change_stock()
+    for row in file2.iter_rows(min_row=2):
+        if row[0].value== item:
+            found = True
+            decision = input(
+                f"Are you sure you want to remove {row[0].value}? (y/n): "
+            ).lower()
+
+            if decision == "y":
+                row[0].value = None
+                row[1].value = None
+                file.save("DatabaseCap.xlsx")
+                print("Item removed successfully.")
+            else:
+                print("Removal cancelled.")
+            break
+
+    if not found:
+        dec = input(f"{item} not found, press r to retry").lower()
+        if dec == "r":
+            remv_item()
+
+for row,row1 in file2.iter_rows(min_row=2,values_only=1):
+    if row==None:
+        continue
+    print(row,row1)
+
+
