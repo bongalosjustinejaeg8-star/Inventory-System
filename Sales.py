5f167c4ad1ec204283e1fd526188b40e21525e24
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-01-16 19:49:00 +0800

    added reciept func

--- Sales.py
+++ Sales.py
@@ -20,9 +20,27 @@ def chkproduct(code):
 def price(code):
     for row in ws1.iter_rows(min_row=1, max_col=5, values_only=True):
         if row[0] == code:
-            return row[4]
+            return row[3]
     return None
-
+def printreciept(sales_id):
+    print("Reciept")
+    print(f"Sales ID: {sales_id}")
+    for row in ws2.iter_rows(min_row=1,values_only=True):
+        if row[0] == sales_id:
+            print(row[2], row[3], row[4], row[5])
+    print("-"*10)
+def update_stock(code, qty_sold):
+    for row in ws1.iter_rows(min_row=2):
+        if str(row[0].value).strip().upper() == code:
+            current_stock = row[4].value
+            new_stock = current_stock - qty_sold
+            if new_stock < 0:
+                print(f"Not enough stock for {code}! Only {current_stock} left.")
+                return False
+            row[4].value = new_stock
+            wb1.save(Inventory_Database)
+            return True
+    return False 
 def buy():
     now = datetime.now()
     sales_id = random.randint(10000, 99999)
@@ -42,14 +60,16 @@ def buy():
         if not chkproduct(code):
             input("Item not found. Press Enter to continue...")
             continue
-
+        
+        update_stock(code,qty)
         p = price(code)
-        subtotal = p * qty
-        ws2.append([sales_id, now.strftime("%Y-%m-%d %H:%M:%S"), code, qty, p, subtotal])
+        subtotal = float(p) * qty
+        ws2.append([sales_id, now.strftime("%Y-%m-%d %H:%M:%S"), code, qty, p, float(subtotal)])
 
     wb2.save("sale.xlsx")
+    wb1.save("Database.xlsx")
+    printreciept(sales_id)
     print("Sale complete!")
 
-buy()
-for row in ws2.iter_rows(values_only=1):
-    print(row)  
+    
+printreciept(31258)
